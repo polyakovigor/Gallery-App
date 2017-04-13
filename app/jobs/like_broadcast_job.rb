c9f009179ec7364231806bedeb0a891eaabb992c
Igor <igor.polyakov@faceit-team.com>
2017-04-13 21:31:53 +0300

    Update like_broadcast_job.rb

--- app/jobs/like_broadcast_job.rb
+++ app/jobs/like_broadcast_job.rb
@@ -2,14 +2,6 @@ class LikeBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(like)
-    ActionCable.server.broadcast "likes_for_image_#{ like.image_id }_channel",
-                                 like: like(like)
+    ActionCable.server.broadcast "likes_for_image_#{ like.image_id }_channel", like.image.likes.size
   end
-
-  private
-
-  def like(like)
-    CommentsController.render '#', locals: { like: like }
-  end
-
 end
