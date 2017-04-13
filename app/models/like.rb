b1f97ab7a6184dbdfb8f2fe91308c50dddebd164
Igor <igor.polyakov@faceit-team.com>
2017-04-13 21:36:03 +0300

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,5 @@ class Like < ApplicationRecord
   belongs_to :image, counter_cache: true
 
   after_create_commit { LikeBroadcastJob.perform_later(self) }
+  after_destroy {  LikeBroadcastJob.perform_later(self) }
 end
