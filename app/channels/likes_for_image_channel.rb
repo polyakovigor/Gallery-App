c7c5e6bd222fed478941001844ce7bb78e06d102
Igor <igor.polyakov@faceit-team.com>
2017-04-13 21:21:29 +0300

    Update likes_for_image_channel.rb

--- app/channels/likes_for_image_channel.rb
+++ app/channels/likes_for_image_channel.rb
@@ -7,10 +7,10 @@ class LikesForImageChannel < ApplicationCable::Channel
   end
 
   def like(data)
-    current_user.likes.create!(body: data['like'], image_id: data['image_id'])
+    current_user.likes.create!(image_id: data['image_id'])
   end
 
   def dislike(data)
-    current_user.likes.destroy!(body: data['dislike'], image_id: data['image_id'])
+    current_user.likes.where(image_id: data['image_id'], user_id: current_user.id)
   end
 end
