8039893bafb3a67c67047a597a5bea5b6149ede9
Igor <igor.polyakov@faceit-team.com>
2017-04-26 18:28:42 +0300

    events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,28 +1,24 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
 
-  # def index
-  #
-  # end
-  #
-  # def navigation
-  #
-  # end
-  #
-  # def user_sign_in
-  #   # @user = User.find(params[:id])
-  #   # @user.last_sign_in_at
-  # end
-  #
-  # def user_sign_out
-  #
-  # end
-  #
-  # def user_likes
-  #
-  # end
-  #
-  # def user_comments
-  #
-  # end
+  def index
+    @users = User.all
+  end
+
+  def navigation
+  end
+
+  def user_sign_in
+    # @user = User.find(params[:id])
+  end
+
+  def user_sign_out
+  end
+
+  def user_likes
+  end
+
+  def user_comments
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,92 +13,92 @@
 ActiveRecord::Schema.define(version: 20170410141052) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
+  create_table 'categories', force: :cascade do |t|
+    t.string   'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer  'user_id'
   end
 
-  create_table "chat_rooms", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_chat_rooms_on_user_id", using: :btree
+  create_table 'chat_rooms', force: :cascade do |t|
+    t.string   'title'
+    t.integer  'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_chat_rooms_on_user_id', using: :btree
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "user_id"
-    t.integer  "image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["image_id"], name: "index_comments_on_image_id", using: :btree
-    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  create_table 'comments', force: :cascade do |t|
+    t.text     'body'
+    t.integer  'user_id'
+    t.integer  'image_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['image_id'], name: 'index_comments_on_image_id', using: :btree
+    t.index ['user_id'], name: 'index_comments_on_user_id', using: :btree
   end
 
-  create_table "images", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "category_id"
-    t.string   "picture"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table 'images', force: :cascade do |t|
+    t.string   'title'
+    t.integer  'category_id'
+    t.string   'picture'
+    t.datetime 'created_at',  null: false
+    t.datetime 'updated_at',  null: false
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["image_id"], name: "index_likes_on_image_id", using: :btree
-    t.index ["user_id", "image_id"], name: "index_likes_on_user_id_and_image_id", using: :btree
-    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
+  create_table 'likes', force: :cascade do |t|
+    t.integer  'user_id'
+    t.integer  'image_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['image_id'], name: 'index_likes_on_image_id', using: :btree
+    t.index ['user_id', 'image_id'], name: 'index_likes_on_user_id_and_image_id', using: :btree
+    t.index ['user_id'], name: 'index_likes_on_user_id', using: :btree
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "user_id"
-    t.integer  "chat_room_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["chat_room_id"], name: "index_messages_on_chat_room_id", using: :btree
-    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+  create_table 'messages', force: :cascade do |t|
+    t.text     'body'
+    t.integer  'user_id'
+    t.integer  'chat_room_id'
+    t.datetime 'created_at',   null: false
+    t.datetime 'updated_at',   null: false
+    t.index ['chat_room_id'], name: 'index_messages_on_chat_room_id', using: :btree
+    t.index ['user_id'], name: 'index_messages_on_user_id', using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "first_name"
-    t.string   "second_name"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  create_table 'users', force: :cascade do |t|
+    t.string   'email',                  default: '', null: false
+    t.string   'encrypted_password',     default: '', null: false
+    t.string   'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.integer  'sign_in_count',          default: 0,  null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.inet     'current_sign_in_ip'
+    t.inet     'last_sign_in_ip'
+    t.string   'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string   'unconfirmed_email'
+    t.integer  'failed_attempts',        default: 0,  null: false
+    t.string   'unlock_token'
+    t.datetime 'locked_at'
+    t.datetime 'created_at',                          null: false
+    t.datetime 'updated_at',                          null: false
+    t.string   'first_name'
+    t.string   'second_name'
+    t.index ['email'], name: 'index_users_on_email', unique: true, using: :btree
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
   end
 
-  add_foreign_key "chat_rooms", "users"
-  add_foreign_key "comments", "images"
-  add_foreign_key "comments", "users"
-  add_foreign_key "likes", "images"
-  add_foreign_key "likes", "users"
-  add_foreign_key "messages", "chat_rooms"
-  add_foreign_key "messages", "users"
+  add_foreign_key 'chat_rooms', 'users'
+  add_foreign_key 'comments', 'images'
+  add_foreign_key 'comments', 'users'
+  add_foreign_key 'likes', 'images'
+  add_foreign_key 'likes', 'users'
+  add_foreign_key 'messages', 'chat_rooms'
+  add_foreign_key 'messages', 'users'
 end
