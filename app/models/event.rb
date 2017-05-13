099937804d0a493c3d7ae5698861dff9d3b1d1a9
Igor <igor.polyakov@faceit-team.com>
2017-05-13 15:03:47 +0300

    event validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user
 
   default_scope { order('created_at desc') }
+
+  validates :user_id, presence: true
+  validates :action, inclusion: { in: %w(Visit SignIn SignOut) }
+  validates :url, presence: true, if: Proc.new{ |event| event.action == 'Visit' }
 end
