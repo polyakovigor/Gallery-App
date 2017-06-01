02809c656983449b86dc7013c0dd30c891a07cca
Igor <igor.polyakov@faceit-team.com>
2017-06-01 15:13:28 +0300

    Add redis config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,11 @@ LikeApp::Application.configure do
       password: ENV['GMAIL_PASSWORD'],
       openssl_verify_mode: 'none'
   }
+
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
 end
