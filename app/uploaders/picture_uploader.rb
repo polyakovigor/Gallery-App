399035d9609b4a8c8a51d044f68b8539ab60d437
Igor <igor.polyakov@faceit-team.com>
2017-06-06 17:20:27 +0300

    updated uploader

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,32 +3,15 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
 
-  # storage :file
-
-  # def cache_dir
-  #   "/tmp/uploads"
-  # end
-  #
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+  storage :file
 
   version :standard do
-    process resize_to_fill: [780, 780, :north]
+    process resize_to_fill: [780, 780]
   end
 
   version :thumbnail do
     process resize_to_fit: [200, 200]
   end
-
-  # version :thumb do
-  #    process resize_to_fill: [200, 200]
-  # end
-  #
-  # version :large do
-  #    process resize_to_fit: [780, 780]
-  #    process resize_to_fill: [780, 780]
-  # end
 
   def extension_white_list
     %w(jpg jpeg gif png)
