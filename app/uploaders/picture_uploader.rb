a065099a4ff19b8d30ea22d91a76646383f26866
Igor <igor.polyakov@faceit-team.com>
2017-06-06 16:18:20 +0300

    configure cloudinary

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,7 +3,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
 
-  storage :file
+  # storage :file
 
   # def cache_dir
   #   "/tmp/uploads"
@@ -13,14 +13,22 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   # end
 
-  version :thumb do
-     process resize_to_fill: [200, 200]
+  version :standard do
+    process resize_to_fill: [780, 780, :north]
   end
 
-  version :large do
-     process resize_to_fit: [780, 780]
-     process resize_to_fill: [780, 780]
+  version :thumbnail do
+    process resize_to_fit: [200, 200]
   end
+
+  # version :thumb do
+  #    process resize_to_fill: [200, 200]
+  # end
+  #
+  # version :large do
+  #    process resize_to_fit: [780, 780]
+  #    process resize_to_fill: [780, 780]
+  # end
 
   def extension_white_list
     %w(jpg jpeg gif png)
