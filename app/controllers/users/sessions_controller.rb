c9421a80f112d34fa04b5c98eb2b75601c792b66
Igor <igor.polyakov@faceit-team.com>
2017-10-30 16:59:15 +0200

    refactor views

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,19 +1,17 @@
-class Users
-  class SessionsController < Devise::SessionsController
-    def create
-      super
-      create_event('SignIn')
-    end
+class Users::SessionsController < Devise::SessionsController
+  def create
+    super
+    create_event('SignIn')
+  end
 
-    def destroy
-      create_event('SignOut')
-      super
-    end
+  def destroy
+    create_event('SignOut')
+    super
+  end
 
-    private
+  private
 
-    def create_event(action)
-      Event.create(user_id: current_user.try(:id), action: action)
-    end
+  def create_event(action)
+    Event.create(user_id: current_user.try(:id), action: action)
   end
 end
