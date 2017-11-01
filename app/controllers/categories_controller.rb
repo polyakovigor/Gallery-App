85fcc3beac76cb9220171ae1c7c0fc6df988624a
Igor <igor.polyakov@faceit-team.com>
2017-11-01 10:25:29 +0200

    add categories#index to authenticate except list

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
   before_action :set_category, only: %i[show destroy]
 
   def index
