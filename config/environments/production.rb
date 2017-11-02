94e313faa1a52b5567ecfc19574ae70a0b67b4fb
Igor <igor.polyakov@faceit-team.com>
2017-11-02 14:28:57 +0200

    fix footer position

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ LikeApp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'polar-mountain-18909.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://polar-mountain-18909.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -87,7 +87,7 @@ LikeApp::Application.configure do
 
   config.action_mailer.smtp_settings = { address:               'smtp.gmail.com',
                                          port:                  587,
-                                         domain:                'gmail.com',
+                                         domain:                'https://polar-mountain-18909.herokuapp.com/',
                                          user_name:             ENV['SENDMAIL_USERNAME'],
                                          password:              ENV['SENDMAIL_PASSWORD'],
                                          authentication:        'plain',
